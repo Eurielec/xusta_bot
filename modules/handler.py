395e791ab7933c76d5b94accf4f2e4333a9a5edb
d3vv3 <jaimecondel@gmail.com>
2021-10-05 02:16:39 +0200

    fix: class

--- modules/handler.py
+++ modules/handler.py
@@ -34,17 +34,18 @@ class Handler:
         self.beeper = beep.Beeper()
         self.covid = covid.Covid()
         self.log = logs.Log()
+        self.filter = filter.Filter()
         return
 
     def get_image_handler(self, message):
-        if filter.check_auth(message) and filter.check_limits(message):
+        if self.filter.check_auth(message) and self.filter.check_limits(message):
             self.log.log_message(message)
             self.beeper.get_image_advertise()
             self.cam.get_picture(message)
         return
 
     def get_video_handler(self, message):
-        if filter.check_auth(message) and filter.check_limits(message):
+        if self.filter.check_auth(message) and self.filter.check_limits(message):
             self.log.log_message(message)
             self.beeper.get_video_advertise()
             ff = self.cam.ffmpeg_video(5)
@@ -53,7 +54,7 @@ class Handler:
         return
 
     def get_salseo_handler(self, message):
-        if filter.check_auth(message) and filter.check_limits(message):
+        if self.filter.check_auth(message) and self.filter.check_limits(message):
             self.log.log_message(message)
             self.beeper.get_salseo_advertise()
             ff = self.cam.ffmpeg_video(15)
@@ -63,7 +64,7 @@ class Handler:
         return
 
     def get_nevera_handler(self, message):
-        if filter.check_auth(message) and filter.check_limits(message):
+        if self.filter.check_auth(message) and self.filter.check_limits(message):
             self.log.log_message(message)
             self.beeper.get_nevera_advertise()
             ff = self.cam.ffmpeg_video(15)
@@ -73,7 +74,7 @@ class Handler:
         return
 
     def reset_camera_handler(self, message):
-        if filter.check_auth(message):
+        if self.filter.check_auth(message):
             self.cam.reset(message)
         return
 
@@ -88,13 +89,13 @@ class Handler:
         return
 
     def cast_handler(self, message):
-        if filter.god_check_auth(message):
+        if self.filter.god_check_auth(message):
             self.log.log_message(message)
             bot.send_message(filter.groups[1], message.text[6:])
         return
 
     def get_current_people_handler(self, message):
-        if filter.check_auth(message):
+        if self.filter.check_auth(message):
             self.log.log_message(message)
             current = self.covid.get_current_people()
             bot.send_message(message.chat.id, current)
@@ -102,14 +103,14 @@ class Handler:
 
     """
     def get_turn_handler(self, message):
-        if filter.check_auth(message) or filter.god_check_auth(message):
+        if self.filter.check_auth(message) or self.filter.god_check_auth(message):
             self.log.log_message(message)
             result = batallon.find_turn()
             bot.send_message(message.chat.id, result)
         return
 
     def turn_done_handler(self, message):
-        if filter.god_check_auth(message):
+        if self.filter.god_check_auth(message):
             self.log.log_message(message)
             bot.send_message(message.chat.id, "Let's see...")
             self.cam.get_basura(message)
@@ -120,7 +121,7 @@ class Handler:
         return
 
     def turn_undo_handler(self, message):
-        if filter.god_check_auth(message):
+        if self.filter.god_check_auth(message):
             self.log.log_message(message)
             batallon.turn_status(False)
             bot.send_message(message.chat.id,
